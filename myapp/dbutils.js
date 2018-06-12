4afa190ca828a1c6835e1876c27a05ee5899093a
manmeet-rana <31946713+manmeet-rana@users.noreply.github.com>
2018-06-12 16:35:12 +0530

    adding transaction

--- myapp/dbutils.js
+++ myapp/dbutils.js
@@ -26,6 +26,17 @@ var vendorSchema = new mongoose.Schema({
 	balance : {type:Number},
 });
 
+var transactionSchema = new mongoose.Schema({
+	emp_id : {type:String},
+	comp_id : {type:Number ,default: null},
+	vendor_id : {type :String , default:null},
+	debit_amt : {type:Number, default : 0},
+	credit_amt : {type:Number, default : 0},
+	remarks : {type:String},
+	date : {$type : "date"},
+});
+
+
 
 	var employee = mongoose.model('employee',employeeSchema);
 	var company = mongoose.model('company',companySchema);
@@ -80,6 +91,23 @@ var vendorSchema = new mongoose.Schema({
 		},
 		showVendor:function(callback){
 			vendor.find({},function (err,res) {
+				if(err)
+					callback(1,null);
+				else
+					callback(0,res);
+			})
+		},
+		newTransaction:function (details,callback) {
+			var data = new transaction(details);
+			data.save(function (err,result) {
+				if(err)
+					callback(1,null);
+				else
+					callback(0,result);
+			});
+		},
+		showTransaction:function(callback){
+			transaction.find({},function (err,res) {
 				if(err)
 					callback(1,null);
 				else
